8d40dd87cc6f3779553eae9b79c4d2f516fcd9b4
RJ-ROSS <motor.madness.industries@gmail.com>
2021-03-01 22:37:57 +0800

    Reformatted image displaying order and new HSV values for the ball

--- Testing/track_ball_video.py
+++ Testing/track_ball_video.py
@@ -4,84 +4,29 @@
 
 # import the necessary packages
 from collections import deque
-from imutils.video import VideoStream
 import numpy as np
 import argparse
 import cv2
 import imutils
 import time
 
-# define the lower and upper boundaries of the "green"
-# ball in the HSV color space, then initialize the
-# list of tracked points
-# ballLower = (7, 88, 164)
-ballLower = (7, 84, 172)
-ballUpper = (179, 255, 255)
-queue_size = 10
+# define the lower and upper boundaries of the ball in the HSV color space,
+# then initialize the list of tracked points
+# ball_HSV_Lower = (7, 84, 172)
+# ball_HSV_Upper = (179, 255, 255)
+ball_HSV_Lower = (12, 72, 157)
+ball_HSV_Upper = (30, 153, 255)
+queue_size = 10# length of tail of tracked ball
 pts = deque(maxlen=queue_size) # quicker append and pop from both ends than lists
 
 def morph_operations(frame):
-    cv2.namedWindow("OG", cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('OG', frame)
-
-    # blur frame, and convert it to the HSV color space
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    cv2.namedWindow("blurred", cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('blurred', blurred)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-    mask = cv2.inRange(hsv, ballLower, ballUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
-    cv2.namedWindow("mask", cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('mask', mask)
-
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-    cv2.namedWindow("result", cv2.WINDOW_AUTOSIZE)
-    cv2.imshow('result', result)
-
-    # find contours in the mask and initialize the current
-    # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    center = None
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        # only proceed if the radius meets a minimum size
-        if radius > 10:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
-        print("none")
-    # update the points queue
-    pts.appendleft(center)
-
-    # loop over the set of tracked points
-    for i in range(1, len(pts)):
-        # if either of the tracked points are None, ignore
-        # them
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-        # otherwise, compute the thickness of the line and
-        # draw the connecting lines
-        thickness = int(np.sqrt(queue_size / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+    pass
 
 
 
 def main():
-    # frame = cv2.imread('C:/Users/Ryan/Documents/SharedUbuntu16.4LTS/BallPID/test_files/ball2_resized.png')
-    cap = cv2.VideoCapture('C:/Users/Ryan/Documents/SharedUbuntu16.4LTS/BallPID/test_files/01.avi')
-    # cap = cv2.VideoCapture('C:/Users/Ryan/Documents/SharedUbuntu16.4LTS/BallPID/test_files/02.avi')
+    # cap = cv2.VideoCapture('C:/Users/Ryan/Documents/SharedUbuntu16.4LTS/BallPID/test_files/01.avi')
+    cap = cv2.VideoCapture('C:/Users/Ryan/Documents/SharedUbuntu16.4LTS/BallPID/test_files/02.avi')
     fps = 0
     func_time = 0
     max_x = 0
@@ -92,18 +37,15 @@ def main():
         if ret:
             start_func = time.clock()
             # morph_operations(frame)
-
+            OG = frame.copy()
             # -----
-            cv2.namedWindow("OG", cv2.WINDOW_AUTOSIZE)
-            cv2.imshow('OG', frame)
-
             # blur frame, and convert it to the HSV color space
             blurred = cv2.GaussianBlur(frame, (11, 11), 0)
             # cv2.namedWindow("blurred", cv2.WINDOW_AUTOSIZE)
             # cv2.imshow('blurred', blurred)
             hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-            mask = cv2.inRange(hsv, ballLower, ballUpper)
+            mask = cv2.inRange(hsv, ball_HSV_Lower, ball_HSV_Upper)
             mask = cv2.erode(mask, None, iterations=2)
             mask = cv2.dilate(mask, None, iterations=2)
             # cv2.namedWindow("mask", cv2.WINDOW_AUTOSIZE)
@@ -117,7 +59,7 @@ def main():
             # (x, y) center of the ball
             cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             cnts = imutils.grab_contours(cnts)
-            center = None
+            center = None #
             # only proceed if at least one contour was found
             if len(cnts) > 0:    #source: https://www.pyimagesearch.com/2016/02/01/opencv-center-of-contour/
                 # find the largest contour in the mask, then use
@@ -136,8 +78,13 @@ def main():
                     min_x = cX
                 mid_x = int((max_x + min_x)/2)
                 # cv2.line(frame, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2), 0), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))), (0, 0, 255), 2)
-                cv2.line(frame, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2)-25, 0), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2)-25, int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))), (0, 0, 255), 1)
+                cv2.line(frame, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2)-25, 0), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)/2)-25, int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))), (255, 229, 128), 1)
                 cv2.line(frame, (mid_x, 0), (mid_x, int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))), (255, 0, 0), 1)
+
+
+                # crosshairs
+                cv2.line(OG, (cX, 0), (cX, int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))), (0, 0, 255), 1)
+                cv2.line(OG, (0, cY), (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), cY), (0, 0, 255), 1)
 
 
 
@@ -155,7 +102,7 @@ def main():
             else:
                 print("none")
             # update the points queue
-            pts.appendleft(center)
+            pts.appendleft(center) #ppends the centroid to the pts  list
 
             # loop over the set of tracked points
             for i in range(1, len(pts)):
@@ -175,14 +122,22 @@ def main():
             cv2.namedWindow("Tracked", cv2.WINDOW_AUTOSIZE)
             cv2.imshow('Tracked', frame)
 
-            # -----
-            end_func = time.clock()
-            func_time = end_func- start_func
-            fps = 1/(func_time)
-            print("Func time: {:.4f} ms".format(float(1000*(func_time))))
+            cv2.namedWindow("OG", cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('OG', OG)
 
-            if (cv2.waitKey(33) & 0xFF) == ord('q'): # Hit `q` to exit
+            # -----
+            # end_func = time.clock()
+            # func_time = end_func- start_func
+            # fps = 1/(func_time)
+            # print("Func time: {:.4f} ms".format(float(1000*(func_time))))
+
+            if (cv2.waitKey(20) & 0xFF) == ord('q'): # Hit `q` to exit
                 break
+
+            end_func = time.clock()
+            func_time = end_func - start_func
+            fps = 1 / (func_time)
+            print("Func time: {:.4f} ms".format(float(1000 * (func_time))))
         else:
             # break
             cap = cv2.VideoCapture('C:/Users/Ryan/Documents/SharedUbuntu16.4LTS/BallPID/test_files/01.avi')
